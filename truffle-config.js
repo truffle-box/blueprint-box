278c988d4df80d5f8aa060948759d10b0a64b20c
Gabriel Cardona <cgcardona@gmail.com>
2020-01-04 16:02:09 -0800

    Comment out line.

--- truffle-config.js
+++ truffle-config.js
@@ -3,7 +3,7 @@ const path = require("path")
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
-  contracts_build_directory: path.join(__dirname, "src/contracts"),
+  // contracts_build_directory: path.join(__dirname, "src/contracts"),
   networks: {
     development: {
       host: "127.0.0.1",
