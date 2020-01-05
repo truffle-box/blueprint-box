853f6aa76f9d78c93cd1c92e80890dacaf65c530
Gabriel Cardona <cgcardona@gmail.com>
2020-01-04 16:00:36 -0800

    update truffle-config.

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,19 @@
+const path = require("path")
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
-};
+  // for more about customizing your Truffle configuration!
+  contracts_build_directory: path.join(__dirname, "src/contracts"),
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.6.1"
+    }
+  }
+}
