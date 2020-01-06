5a728ab8c87e3cc3bc1b575c9f9986595e5b77af
Gabriel Cardona <cgcardona@gmail.com>
2020-01-06 11:58:48 -0800

    Clean up truffle config and add Solidity syntax highlighting.

--- truffle-config.js
+++ truffle-config.js
@@ -2,18 +2,5 @@ const path = require("path")
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
-  // for more about customizing your Truffle configuration!
-  // contracts_build_directory: path.join(__dirname, "src/contracts"),
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 8545,
-      network_id: "*" // Match any network id
-    }
-  },
-  compilers: {
-    solc: {
-      version: "0.6.1"
-    }
-  }
+  // to customize your Truffle configuration!
 }
